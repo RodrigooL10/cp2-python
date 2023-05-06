883b170afedee88177033e0fc74a530f38ee96d4
Rodrigo Lima <128654680+RodrigooL10@users.noreply.github.com>
2023-05-05 21:09:19 -0300

    Update cp2.py

--- cp2.py
+++ cp2.py
@@ -1,26 +1,31 @@
-# -*- coding: utf-8 -*-
+""" Grupo:
+    - Guilherme Dejulio - RM550295
+    - Marcelo Tan - RM551771
+    - Rodrigo Lima - RM98326
+"""
+
 import sys
 from sys import exit
 
-# criando tipos de vinho, quantidade e preços
-
-estoque = [["Vinho tinto", 50, 35.90],
-           ["Vinho branco", 20, 29.99],
-           ["Vinho rosé", 15, 24.50],
-           ["Vinho espumante", 26, 42.00],
-           ["Vinho do porto", 45, 55.00]]
+# criando estoque com alguns produtos e suas quantidades
+estoque = [["Garrafa tipo A", 500],
+           ["Garrrafa tipo B", 200],
+           ["Rolha", 150],
+           ["Rótulo", 260],
+           ["Uva", 450]]
 
 compras = []
 
 id_compra = 0
 
-# criando menu
+# criando menu principal
 while True:
-    print("----- MENU PRINCIPAL -----")
+    print("\n----- MENU PRINCIPAL -----")
     print("1. Menu de Estoque")
     print("2. Menu de Compras")
     menu = int(input("Digite qual menu deseja acessar: "))
-# criando menu para escolha estoque
+
+    # criando menu de estoque
     if menu == 1:
         while True:
             print("\n----- Menu de Estoque -----")
@@ -29,93 +34,104 @@ while True:
             print("3. Registrar Saída")
             print("0. Voltar ao Menu Principal")
             menu2 = int(input("Digite qual opção deseja acessar: "))
-# exibindo estoque
+
+            # exibindo estoque
             match menu2:
                 case 1:
-                    print("----- Estoque -----")
-                    print("Produto | Quantidade | Valor")
+                    print("\n----- Estoque -----")
+                    print("Produto | Quantidade ")
 
-                    for vinho, quantidade, preco in estoque:
-                        print(f"{vinho} | {quantidade} | R${preco:.2f}")
+                    for produto, quantidade in estoque:
+                        print(f"{produto} | {quantidade}")
 
                     print("----- Fim do Estoque -----\n")
-# criando codigo para adicionar produtos no estoque
+
+                # criando codigo para adicionar produtos no estoque
                 case 2:
-                    print("----- Registrar Entrada -----")
+                    print("\n----- Registrar Entrada -----")
                     nome = input("Digite o nome do produto: ")
                     quantidade = int(input("Digite a quantidade: "))
 
                     for i in range(len(estoque)):
-                        vinho = estoque[i][0]
-                        qtd_vinho = estoque[i][1]
+                        produto = estoque[i][0]
+                        qtd_produto = estoque[i][1]
 
-                        if vinho == nome:
-                            qtd_vinho += quantidade
-                            estoque[i][1] = qtd_vinho
-                            print(estoque)
+                        if produto == nome:
+                            qtd_produto += quantidade
+                            estoque[i][1] = qtd_produto
                             break
                         else:
-                            valor = float(input("Digite o valor do produto: "))
-                            estoque.append([nome,quantidade,valor])
-                            print(estoque)
+                            estoque.append([nome,quantidade])
                             break
+                    print("Produto Registrado com sucesso")
+                    print("----- Fim do Registro de Entrada -----")
 
-                    print("\n----- Fim do Registro de Entrada -----\n")
-# criando codigo para retirada de produtos no estoque 
+                # criando codigo para retirada de produtos no estoque
                 case 3:
-                    print("----- Registrar Saída -----")
+                    print("\n----- Registrar Saída -----")
                     nome = input("Digite o nome do produto: ")
-                    quantidade = int(input("Digite a quantidade: "))
-
                     encontrou_item = False
+
                     for i in range(len(estoque)):
-                        vinho = estoque[i][0]
-                        qtd_vinho = estoque[i][1]
+                        produto = estoque[i][0]
+                        qtd_produto = estoque[i][1]
 
-                        if vinho == nome:
-                            qtd_vinho -= quantidade
-                            estoque[i][1] = qtd_vinho
-                            print(estoque)
+                        if produto == nome:
+                            quantidade = int(input("Digite a quantidade: "))
+                            qtd_produto -= quantidade
+                            estoque[i][1] = qtd_produto
                             encontrou_item = True
+                            print("A quantidade de produto foi removida com sucesso")
+                            print("\n----- Fim do Registro de Saída -----")
                             break
-
 
                     if not encontrou_item:
-                        print("Produto não encontrado no estoque")
-
-                    print("\n----- Fim do Registro de Saída -----\n")
-
+                        print("Produto não foi encontrado no estoque")
+                #Voltando ao menu principal
                 case 0:
-                    print("Voltando ao menu principal...\n")
+                    print("\nVoltando ao menu principal...")
                     break
 
-# criando codigo para compra e carrinho
+    # criando menu de compras
     elif menu == 2:
         while True:
-            print("----- Menu de Compras -----")
+            print("\n----- Menu de Compras -----")
             print("1. Cadastrar compra")
             print("2. Mostrar todas as compras")
+            print("0. Voltar ao Menu Principal")
             opcao_compras = int(input("Digite a opção desejada: "))
 
+            match opcao_compras:
 
-            if opcao_compras == 1:
-                cnpj_fornecedor = input("Digite o CNPJ do fornecedor: ")
-                descricao = input("Digite a descrição da compra: ")
-                quantidade = int(input("Digite a quantidade da compra: "))
-                valor_unitario = float(input("Digite o valor unitário da compra: "))
-                valor_total = quantidade * valor_unitario
-                id_compra += 1
-                compra = {"cnpj_fornecedor": cnpj_fornecedor, "descricao": descricao,
-                          "quantidade": quantidade, "valor_unitario": valor_unitario,
-                          "valor_total": valor_total}
-                compras.append(compra)
+                # Cadastrando compra
+                case 1:
+                    print("\n-- Cadastrando compra --")
+                    cnpj_fornecedor = input("Digite o CNPJ do fornecedor: ")
+                    descricao = input("Digite a descrição da compra: ")
+                    quantidade = int(input("Digite a quantidade da compra: "))
+                    valor_unitario = float(input("Digite o valor unitário da compra: "))
+                    valor_total = quantidade * valor_unitario
+                    id_compra += 1
+                    compra = {"id": id_compra, "cnpj_fornecedor": cnpj_fornecedor, "descricao": descricao,
+                              "quantidade": quantidade, "valor_unitario": valor_unitario,
+                              "valor_total": valor_total}
+                    compras.append(compra)
+                    print("Compra foi cadastrada com sucesso!")
+                    print("\nVoltando ao menu de compras...")
 
-            elif opcao_compras == 2:
-                print("Todas as compras:")
-                for compra in compras:
-                    print(f"--Id da Compra: {id_compra}--")
-                    print("CNPJ do fornecedor:", compra["cnpj_fornecedor"])
-                    print("Descrição:", compra["descricao"])
-                    print("Quantidade:", compra["quantidade"])
-                    print("Valor unitário:", compra["valor_unitario"])
-                    print("Valor total:", compra["valor_total"])
+                #Exibindo compras realizadas
+                case 2:
+                    print("\n-- Exibindo todas as compras: --")
+                    for compra in compras:
+                        print("\n-- Id da Compra:", compra["id"], "--")
+                        print("CNPJ do fornecedor:", compra["cnpj_fornecedor"])
+                        print("Descrição:", compra["descricao"])
+                        print("Quantidade:", compra["quantidade"])
+                        print("Valor unitário:", compra["valor_unitario"])
+                        print("Valor total:", compra["valor_total"])
+                    print("\n-- Fim de todas as compras realizadas --")
+
+                #Voltando ao menu principal
+                case 0:
+                    print("\nVoltando ao menu principal...")
+                    break
